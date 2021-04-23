272331ec5712f8dba7c05b0f2e97b93fb47402d6
麦冬 <380726211@qq.com>
2021-04-23 15:50:33 +0800

    修改

--- src/UFX.ExcelIE.Application.Contacts/Dtos/Export/SysConfig.cs
+++ src/UFX.ExcelIE.Application.Contacts/Dtos/Export/SysConfig.cs
@@ -8,10 +8,17 @@ namespace UFX.ExcelIE.Application.Contracts.Dtos.Export
 {
     public class SysConfig
     {
-        public ExcelEDownLoad ExcelEDownLoad{ get; set; }
+        public ExcelEDownLoad ExcelEDownLoad { get; set; }
     }
-    public class ExcelEDownLoad {
-        public string LocalUrl { get; set; }
+    public class ExcelEDownLoad
+    {
+        /// <summary>
+        /// 布置方式：0-本地，1-远程
+        /// </summary>
+        public int DeployType { get; set; } = 0;
+        /// <summary>
+        /// RemoteUrl
+        /// </summary>
         public string RemoteUrl { get; set; }
     }
 }
